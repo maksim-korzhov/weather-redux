3417e0c81ebfd7bd8ec5facd62632a29261cbbf4
Maksim Korzhov <isitbux@gmail.com>
2017-10-02 22:13:01 +0300

    Added charts. Added styles to input and charts. Finished weather list.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,57 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import Chart from "../components/chart";
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
     constructor(props) {
         super(props);
 
         this.state = {};
     }
 
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+                <td>
+                    <Chart data={temps} color="orange" units="K" />
+                </td>
+                <td>
+                    <Chart data={pressures} color="green" units="hPa" />
+                </td>
+                <td>
+                    <Chart data={humidities} color="black" units="%" />
+                </td>
+            </tr>
+        );
+    }
+
     render() {
         return (
             <table className="table table-hover">
-                <thead></thead>
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
             </table>
         );
     }
 }
+
+function mapStateToProps({weather}) {
+    return {weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import ReduxPromise from "redux-promise";
 import App from "./components/app";
 import reducers from "./reducers";
 
+import style from "../style/style.scss";
+
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
